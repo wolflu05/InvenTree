8f05d1a39a897c812a3e9c334c2ab66371619313
wolflu05 <76838159+wolflu05@users.noreply.github.com>
2024-03-01 14:45:37 +0100

    store page settings in template and detect template that were not created by the label designer

--- src/frontend/src/components/editors/TemplateEditor/LabelEditor/LabelEditor.tsx
+++ src/frontend/src/components/editors/TemplateEditor/LabelEditor/LabelEditor.tsx
@@ -1,7 +1,14 @@
-import { t } from '@lingui/macro';
-import { Stack } from '@mantine/core';
+import { Trans, t } from '@lingui/macro';
+import { Alert, Button, Group, Overlay, Stack, Text } from '@mantine/core';
+import { IconAlertTriangle } from '@tabler/icons-react';
 import Split from '@uiw/react-split';
-import { forwardRef, useEffect, useImperativeHandle, useRef } from 'react';
+import {
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState
+} from 'react';
 
 import { EditorComponent } from '../TemplateEditor';
 import { EditorArea } from './EditorArea';
@@ -19,6 +26,8 @@ import { unitToPixel } from './utils';
 export const LabelEditorComponentE: EditorComponent = forwardRef(
   (props, ref) => {
     const { template } = props as { template: any };
+    const codeRef = useRef<string>();
+    const [active, setActive] = useState<boolean>();
 
     const storeRef = useRef<LabelEditorStore>();
     if (!storeRef.current) {
@@ -39,10 +48,27 @@ export const LabelEditorComponentE: EditorComponent = forwardRef(
         const template = code.match(
           /--- Start template ---\n(.+?)\n--- End template ---/s
         );
-        if (!template) throw new Error(t`Error parsing template`);
-        storeRef.current?.setState({ templateStr: template[1] });
+        if (!template) {
+          codeRef.current = code;
+          setActive(false);
+          return;
+        }
+        codeRef.current = undefined;
+        setActive(true);
+        const restoreState = { templateStr: template[1] } as Record<
+          string,
+          any
+        >;
+        const data = JSON.parse(template[1]);
+        if (data.pageSettings) {
+          restoreState.pageSettings = data.pageSettings;
+        }
+        storeRef.current?.setState(restoreState);
       },
       getCode: () => {
+        // label designer was never active
+        if (codeRef.current) return codeRef.current;
+
         const s = storeRef.current?.getState();
         const data = s?.editor?.canvas?.toObject();
         if (!data) throw new Error(t`Error getting data from canvas`);
@@ -74,7 +100,7 @@ export const LabelEditorComponentE: EditorComponent = forwardRef(
           '==========================================================',
           '',
           '--- Start template ---',
-          JSON.stringify(data),
+          JSON.stringify({ ...data, pageSettings: s?.pageSettings }),
           '--- End template ---',
           '{% endcomment %}',
           '',
@@ -90,7 +116,7 @@ export const LabelEditorComponentE: EditorComponent = forwardRef(
 
     return (
       <LabelEditorContext.Provider value={storeRef.current}>
-        <Stack style={{ display: 'flex', flex: 1 }}>
+        <Stack style={{ display: 'flex', flex: 1, position: 'relative' }}>
           <div style={{ display: 'flex', flexWrap: 'nowrap', flex: 1 }}>
             <LeftPanel />
             <Split style={{ flex: 1 }}>
@@ -99,6 +125,45 @@ export const LabelEditorComponentE: EditorComponent = forwardRef(
             </Split>
           </div>
           <FooterPanel />
+          {active === false && (
+            <Overlay center>
+              <Alert
+                icon={<IconAlertTriangle size="1rem" />}
+                title=""
+                color="red"
+                maw={400}
+              >
+                <Stack spacing="xs">
+                  <Text>
+                    <Trans>
+                      This doesn't look like a valid label designer template,
+                      are you sure you want to continue?
+                    </Trans>
+                  </Text>
+                  <Text>
+                    <Trans>THE COMPLETE TEMPLATE WILL BE LOST!</Trans>
+                  </Text>
+                  <Text>
+                    <Trans>Consider going back to the code editor!</Trans>
+                  </Text>
+                </Stack>
+
+                <Group position="right">
+                  <Button
+                    color="red"
+                    mt={20}
+                    uppercase
+                    onClick={() => {
+                      codeRef.current = undefined;
+                      setActive(true);
+                    }}
+                  >
+                    <Trans>Continue and delete</Trans>
+                  </Button>
+                </Group>
+              </Alert>
+            </Overlay>
+          )}
         </Stack>
       </LabelEditorContext.Provider>
     );
